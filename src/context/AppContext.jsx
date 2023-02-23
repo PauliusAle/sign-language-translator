0d7a6d23916a33ea4535534e281ad2d4d21f0257
Paulius Aleksandravicius <Paulius.Aleksandravicius@no.experis.com>
2023-02-23 14:13:14 +0100

    documentation

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import UserProvider from "./UserContext";
 
+//Function that lets all components passed as children use the user state.
 function AppContext({ children }) {
   return <UserProvider>{children}</UserProvider>;
 }
--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -2,20 +2,25 @@ import { createContext, useState, useContext } from "react";
 import { STORAGE_KEY_USER } from "../const/storageKeys";
 import { storageRead } from "../utils/storage";
 
-//context => exposing state
-const UserContext = createContext(); //!!
+//Create consumer-provider
+const UserContext = createContext(); //
 
+//Function that will return state object - similar to useSelector in react-redux.
 export const useUser = () => {
   return useContext(UserContext);
 };
 
 const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(storageRead(STORAGE_KEY_USER));
+  //declaring state and setting user default value to the one in local storage, if nothing there - null.
+  const [user, setUser] = useState(storageRead(STORAGE_KEY_USER)); 
 
+  //state values
   const state = {
     user,
     setUser,
   };
+
+  //Sett provider value to state and all children will have access to it.
   return <UserContext.Provider value={state}>{children}</UserContext.Provider>;
 };
 
